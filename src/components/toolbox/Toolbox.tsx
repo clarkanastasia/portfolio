b9e46336c5c4b86afd1fbd19e1f88c75d111afcb
Anastasia C <clarkanastasia@outlook.com>
2024-04-22 11:55:38 +0100

    bug fixes

--- src/components/toolbox/Toolbox.tsx
+++ src/components/toolbox/Toolbox.tsx
@@ -1,4 +1,10 @@
 import './Toolbox.scss';
+import c from "../../assets/c.svg"
+import html from "../../assets/html.svg"
+import javascript from "../../assets/javascript.svg"
+import react from "../../assets/react.svg"
+import sass from "../../assets/sass.svg"
+import typescript from "../../assets/typescript.svg"
 
 const Toolbox: React.FC = () => {
 
@@ -6,12 +12,12 @@ const Toolbox: React.FC = () => {
     <div className="toolboxContainer" id="techStack">
         <h1>My Toolbox</h1>
         <div className ="iconsContainer">
-            <img src="src\assets\c.svg" alt="c sharp logo" />
-            <img src="src\assets\html.svg" alt="html logo" />
-            <img src="src\assets\javascript.svg" alt="javascript logo" />
-            <img src="src\assets\react.svg" alt="react logo" />
-            <img src="src\assets\sass.svg" alt="sass logo" />
-            <img src="src\assets\typescript.svg" alt="typescript logo" />
+            <img src={c} alt="c sharp logo" />
+            <img src={html} alt="html logo" />
+            <img src={javascript} alt="javascript logo" />
+            <img src={react} alt="react logo" />
+            <img src={sass} alt="sass logo" />
+            <img src={typescript} alt="typescript logo" />
         </div>
     </div>
     )
