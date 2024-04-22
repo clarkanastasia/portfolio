aa14085a5741602452f303e0d3438485767d219c
Anastasia C <clarkanastasia@outlook.com>
2024-04-22 12:01:04 +0100

    update navbar link

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -4,10 +4,10 @@ const NavBar: React.FC = () => {
 
     return(
     <nav className="navContainer">
-        <a id="pageTitle" href="/">Anastasia Clark</a>
+        <a id="pageTitle" href="/portfolio">Anastasia Clark</a>
 
         <div className="linksContainer">
-            <a href="/">Home</a>
+            <a href="/portfolio">Home</a>
             <a href="#about">About</a>
             <a href="#techStack">Skills</a>
             <a href="#projects">Projects</a>
